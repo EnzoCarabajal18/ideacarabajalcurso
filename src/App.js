a5d149b19d26214f0b9e3cc6b085102da484aab1
Enzo <enzocarabajal7513@gmail.com>
2022-08-24 23:05:22 -0300

    Primera entrega

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import logo from './logo.svg';
+import images from './images/logo-1.jpg';
+
 import './App.css';
 
-function App() {
+const App=()=>{
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p className="App-link">MENDOZA VIAJES</p>
+      <img src={images}/>
     </div>
   );
 }
