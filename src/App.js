d4b9783941f41ed109c4fd35f97eba497d8947a5
EnzoCarabajal18 <105323155+EnzoCarabajal18@users.noreply.github.com>
2022-08-27 13:38:57 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import images from './images/logo-1.jpg';
+
+import './App.css';
+
+const App=()=>{
+  return (
+    <div className="App">
+      <p className="App-link">MENDOZA VIAJES</p>
+      <img src={images}/>
+    </div>
+  );
+}
+
+export default App;
